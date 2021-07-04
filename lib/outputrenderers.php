aa4ebcf18d18e53b12bd3217982ac662f76cbb76
Rajnish Dahiya <cserajnishdahiya@gmail.com>
2021-07-04 19:40:30 +0530

    Revert "Site Logo Hyperlink"
    This reverts commit c7d244c3f0a618c1b3df5be62f3015d002f1cb62.

--- lib/outputrenderers.php
+++ lib/outputrenderers.php
@@ -1195,8 +1195,7 @@ class core_renderer extends renderer_base {
         if ($this->page->pagetype == 'site-index') {
             // Special case for site home page - please do not remove
             return '<div class="sitelink">' .
-                   // '<a title="Moodle" class="d-inline-block aalink" href="http://moodle.org/">' .
-                   '<a title="Moodle" class="d-inline-block aalink" href="http://13.235.49.1/public/">' .
+                   '<a title="Moodle" class="d-inline-block aalink" href="http://moodle.org/">' .
                    '<img src="' . $this->image_url('moodlelogo_grayhat') . '" alt="'.get_string('moodlelogo').'" /></a></div>';
 
         } else if (!empty($CFG->target_release) && $CFG->target_release != $CFG->release) {
